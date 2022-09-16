43f0f96362ff43bee9b8b9655d69badccf6ded5d
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-09-16 18:32:05 -0500

    Programando rutas e importamos componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+
+// Estilos
+import './styles/navbar.css';
+import './styles/forms.css';
 
 export const App = () => {
   return (
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='login' element={<Login />} />
+    </Routes>
   );
 };
